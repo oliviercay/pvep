0de76b2044ac3699e70429a2c4c4f739f57aa26a
oliviercay <ocayatte@gmail.com>
2018-02-01 15:27:03 +0100

    gestion des erreurs

--- analysis/csv.py
+++ analysis/csv.py
@@ -6,10 +6,14 @@ def launch_analysis(data_file):
     directory = os.path.dirname(os.path.dirname(__file__)) # we get the right path.
     path_to_file = os.path.join(directory, "data", data_file) # with this path, we go inside the folder `data` and get the file.
 
-    with open(path_to_file,"r") as f:
-        preview = f.readline()
-
-    print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
+    try:
+        with open(path_to_file,"r") as f:
+            preview = f.readline()
+            print("Yeah! We managed to read the file. Here is a preview: {}".format(preview))
+    except FileNotFoundError as e:
+        raise
+    except:
+        print('Destination unknown')
 
 if __name__ == "__main__":
     launch_analysis('current_mps.csv')
--- parite.py
+++ parite.py
@@ -3,17 +3,36 @@
 import analysis.xml as x_an
 import analysis.csv as c_an
 import argparse
+import logging as lg
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML?""")
     parser.add_argument("-d","--datafile",help="""CSV file containing pieces of information about the members of parliament""")
+    parser.add_argument("-v", "--verbose", action='store_true', help="""Make the application talk!""")
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_arguments()
-    datafile = args.datafile
-    if args.extension == 'xml':
-        x_an.launch_analysis(datafile)
-    elif args.extension == 'csv':
-        c_an.launch_analysis(datafile)
+    if args.verbose:
+        lg.basicConfig(level=lg.DEBUG)
+    try:
+        datafile = args.datafile
+        extension = args.extension
+        if not datafile :
+            raise Warning('You must indicate a datafile!')
+        elif not extension:
+            raise Warning('You must indicate an extension')
+        else:
+            try:
+                if extension == 'xml':
+                    x_an.launch_analysis(datafile)
+                elif extension == 'csv':
+                    c_an.launch_analysis(datafile)
+            except FileNotFoundError as e:
+                lg.error("Ow :( The file was not found.")
+            finally:
+                lg.info('#################### Analysis is over ######################')
+    except Warning as e:
+        lg.warning(e)
+        
